ede02a3f821a2e9ce8645775038beb5cd4de855e
Prashant G Rathod <prashant16gr@gmail.com>
2024-09-26 14:49:39 +0200

    Update Track.cs

--- RetrainingScheduler/Track.cs
+++ RetrainingScheduler/Track.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,13 +12,17 @@ public class Track
 	public Session MorningSession { get; private set; }
 	public Session AfternoonSession { get; private set; }
 
+	// Constructor
 	public Track(int trackNumber)
 	{
 		TrackNumber = trackNumber;
+		// Morning session is 180 minutes (3 hours)
 		MorningSession = new Session(180, new DateTime(1, 1, 1, 9, 0, 0));  // 9:00 AM - 12:00 PM
-		AfternoonSession = new Session(240, new DateTime(1, 1, 1, 13, 0, 0)); // 1:00 PM - 4:00 PM
+		// Afternoon session is 240 minutes (4 hours)
+		AfternoonSession = new Session(240, new DateTime(1, 1, 1, 13, 0, 0)); // 1:00 PM - 5:00 PM
 	}
 
+	// Add talk to track
 	public void PrintTrack()
 	{
 		Console.WriteLine($"Track {TrackNumber}");
